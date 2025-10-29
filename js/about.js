dc7502cc90c28f7699a6cc771aa4980a2fb2a432
Bareera <bgulraiz@uci.edu>
2025-10-29 12:09:50 -0700

    Added the earliest and latest date for the tweets.

--- js/about.js
+++ js/about.js
@@ -11,10 +11,25 @@ function parseTweets(runkeeper_tweets) {
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+	
+	let earliest = tweet_array[0].time;
+	let latest = tweet_array[0].time;
+
+	for (let i = 0 ; i<tweet_array.length; i++){
+		const t = tweet_array[i];
+		if (t.time < earliest) earliest = t.time;
+		if (t.time > latest) latest = t.time;
+
+	}
+
+	const format = {weekday: 'long', month: 'long', day:'numeric', year: 'numeric'}
+
+	document.getElementById('firstDate').innerText = earliest.toLocaleDateString('en-US', format)
+	document.getElementById('lastDate').innerText = latest.toLocaleDateString('en-US', format)
 }
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
 	loadSavedRunkeeperTweets().then(parseTweets);
-});
+}); 

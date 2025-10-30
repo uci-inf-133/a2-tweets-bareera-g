77551ef1d5685b6e6aa8a8caadf22e1d18eb5dee
Bareera <bgulraiz@uci.edu>
2025-10-30 16:54:25 -0700

    Implementation of first two functions of tweet.ts

--- ts/tweet.ts
+++ ts/tweet.ts
@@ -9,14 +9,29 @@ class Tweet {
 
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
+
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        const t = this.text.toLowerCase().replace(/\s+/g, ' ').trim();
+        // completed event
+        if (t.startsWith('just completed') && t.includes('runkeeper') || t.startsWith('just posted') && t.includes('@runkeeper') || t.includes('completed')){
+            return 'completed_event';
+        }
+        // achievement
+        if (t.startsWith('achieved') || t.includes('achieved') || t.includes('personal record') || t.includes('new record') || t.includes('goals') || t.includes('personal best')|| t.includes('set a goal')){
+            return 'achievement'
+        }
+        // live event
+        if (t.startsWith('watch my') || t.includes('live') || t.startsWith('i am') || t.startsWith("i'm") || t.startsWith('biking') || t.startsWith('running') || t.startsWith('walking') || t.startsWith('im ')){
+            return 'live_event'
+        }
+
+        return 'miscellaneous';
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written():boolean {
         //TODO: identify whether the tweet is written
-        return false;
+        return this.text.length > 0;
     }
 
     get writtenText():string {
